b6644d178ff699ef34326a6bf09d1c4ba869bb21
Your Name <sandeep080@gmail.com>
2012-02-15 00:34:17 +0530

    working code... changed QMainWindow to QWidget and made appropriate changes

--- myapplication.py
+++ myapplication.py
@@ -2,16 +2,31 @@ import sys
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
-class MyApplication(QMainWindow):
+class MyApplication(QWidget):
    def __init__(self):
-	  QMainWindow.__init__(self)
-	  self.mainWindow = QMainWindow(self)
-	  self.layout = QVBoxLayout()
-	  self.layout.addWidget(QLabel("HelloWorld"))
-	  self.layout.addWidget(QLineEdit("hello"))
-	  self.mainWindow.setLayout(self.layout)
-	  self.setGeometry(300, 300, 250, 500)
-	  self.setWindowTitle('Icon')
-	  self.setWindowIcon(QIcon('web.png'))
-	  self.setCentralWidget(self.mainWindow)
+	  #The the super class constructor
+	  QWidget.__init__(self) 
+	  
+	  #set the layout of the current widget
+	  self.layout = QVBoxLayout(self)
+
+	  #creating widgets & adding them to the layout created
+	  self.label = QLabel("GSoC",self)
+	  self.layout.addWidget(self.label)
+
+	  self.edit = QLineEdit("KDE",self)
+	  self.layout.addWidget(self.edit)
+
+	  #a single line like this can create and any widget to the 
+	  #layout provided we no more need the reference to it
+	  self.layout.addWidget(QPushButton("hello",self))
+	  
+	  #set the geometry if required
+	  #self.setGeometry(300, 300, 250, 500)
+
+	  #setting window title
+	  #self.setWindowTitle('Icon')
+
+	  #setting window icon
+	  #self.setWindowIcon(QIcon('web.png'))
    
